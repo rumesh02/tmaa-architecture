909e20e181eb5916879ad4f7e899aed3fc32ff73
Rumesh Thisaranga Elpitiya <rumeshth20@gmail.com>
2026-06-19 14:03:37 +0530

    setup with react + tailwind css

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-white">
+      <h1 className="text-4xl font-light tracking-widest uppercase text-stone-800">
+        TMAA Architects
+      </h1>
+    </div>
+  )
+}
+
+export default App
